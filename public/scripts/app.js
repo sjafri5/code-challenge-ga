3cdbc7d75630b5fa1440754aed8437b94c284d66
sjafri5 <raza.jafri.sy@gmail.com>
2016-12-31 11:06:42 -0600

    commented appjs and index.html fg

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,11 @@ requirejs.config({
   baseUrl: './scripts/'
 });
 
+
+// in order for us to import dependant modules we need to wrap all of our js in the requirejs function. the first argument is an array of the names of the dependant files, the second is a function where we will write our code.
+
+// Here, I import two modules: MovieForm and Favorites. Then I call a function on each of them. Both of them seem to be listeners judging by their names.
+
 requirejs(['movie-form', 'favorites'],
 function   (MovieForm, Favorites) {
   Favorites.linkListener();
