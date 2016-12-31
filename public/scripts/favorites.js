decd2c7f5c3d6a587873a99c81c60c66fa9e5f6d
sjafri5 <raza.jafri.sy@gmail.com>
2016-12-31 11:37:44 -0600

    commented movieForm

--- public/scripts/favorites.js
+++ public/scripts/favorites.js
@@ -1,64 +1,86 @@
 define(function (require) {
-  // We are requiring any dependancies that Favorites will have at the top.
+  //  We are requiring any dependancies that Favorites will have at the top.
+
   var Api = require('api')
-  var PageManager= require('page-manager')
-  var FavoriteCard= require('favorite-card')
+    var PageManager= require('page-manager')
+    var FavoriteCard= require('favorite-card')
 
 
 
-  
-  // Here, we are create an Immediately Invoked Function Expression (IIFE). This will allow us to create methods inside the functions but then we can select which of those functions we want to expose based on what we return. 
-  // Since every function has a return value, which is the final output of that function, that is what will be exposed. Can you figure out which of Favorites' methods are exposed / callable. (hint: check the return method)
-  
-  var Favorites= (function(){
-    // linkListener is an event listener. It's searching the document for any element with an id of favorites and listening for a click event. If that click event happens, it runs the function that proceeds
-    
-    var linkListener = function(){
-      document.getElementById("favorites").onclick = function() { 
-        // so if the click happens, it runs the following two lines:
-        //   it calls a f(x) on the PageManager module which seems to clean the display
-        PageManager.clearDisplayContainer();
-        // and it makes an internal call to fetchFavorites
-        fetchFavorites()
+
+    // Here, we are create an Immediately Invoked Function Expression (IIFE). 
+    // This will allow us to create methods inside the functions but then we 
+    // can select which of those functions we want to expose based on what we return. 
+    //
+    // Since every function has a return value, which is the final output of that 
+    // function, that is what will be exposed. Can you figure out which of Favorites' 
+    // methods are exposed / callable. (hint: check the return method)
+
+    var Favorites= (function(){
+
+      // linkListener is an event listener. It's searching the document for 
+      // any element with an id of favorites and listening for a click event. 
+      // If that click event happens, it runs the function that proceeds
+
+      var linkListener = function(){
+        document.getElementById("favorites").onclick = function() { 
+
+          //  so if the click happens, it runs the following two lines:
+          //  it calls a f(x) on the PageManager module which seems to clean the display
+          //  and it makes an internal call to fetchFavorites
+          PageManager.clearDisplayContainer();
+          fetchFavorites()
+        };
       };
-    };
 
-    
-    var fetchFavorites = function(){
-      // fetchFavorites makes the Api module call a function by the same name and then waits for a promise (indicated by the then). upon succesful return of the promise it runs the function encapsulated by the then. If the promise we're to fail then it would skip over then and be deposited in the catch method (not fleshed out below)
+
+      var fetchFavorites = function(){
+        //  fetchFavorites makes the Api module call a function by the same 
+        //  name and then waits for a promise (indicated by the then). upon 
+        //  succesful return of the promise it runs the function encapsulated 
+        //  by the then. If the promise we're to fail then it would skip over 
+        //  then and be deposited in the catch method (not fleshed out below).
+
+        Api.fetchFavorites().then(function(response){
+          //  after succesful return of the promise we call cleanMovieValues and 
+          //  assign the results to a variable. we then call injectFavoriteCards on that var.
+          var favorites = cleanMovieValues(response)
+            injectFavoriteCards(favorites)
+        }).catch(function(err){
+          console.log('error', err);
+        })
+      }
+
+      //  cleanMoviewValues simply maps the array of movie objects and updates 
+      //  their keys so that they are congruent with the keys that omdbapi uses. 
+      //  This way, we can deal with the movie objects coming from the api and the 
+      //  movie objects coming from out storage in the same way. 
       
-      Api.fetchFavorites().then(function(response){
-        // after succesful return of the promise we call cleanMovieValues and assign the results to a variable. we then call injectFavoriteCards on that var.
-        var favorites = cleanMovieValues(response)
-        injectFavoriteCards(favorites)
-      }).catch(function(err){
-        console.log('error', err);
-      })
-    }
+      var cleanMovieValues = function(favorites){
+        return favorites.map(function(favorite){
+          return {
+            Title: favorite.name,
+            imdbID: favorite.oid
+          }
+        });
+      }
 
-    // cleanMoviewValues simply maps the array of movie objects and updates their keys so that they are congruent with the keys that omdbapi uses. This way, we can deal with the movie objects coming from the api and the movie objects coming from out storage in the same way. 
-    var cleanMovieValues = function(favorites){
-      return favorites.map(function(favorite){
-        return {
-          Title: favorite.name,
-          imdbID: favorite.oid
-        }
-      });
-    }
+      //  the method below takes a list of favorite movies and instantiates an 
+      //  instance of FavoriteCard. It then calls initialize on the instance.
+      
+      var injectFavoriteCards = function(favorites){
+        favorites.forEach(function(favorite){
+          var favoriteCard= new FavoriteCard(favorite);
+          favoriteCard.initialize();
+        })
+      }
 
-    // the method below takes a list of favorite movies and instantiates an instance of FavoriteCard. It then calls initialize on the instance.
-    var injectFavoriteCards = function(favorites){
-      favorites.forEach(function(favorite){
-        var favoriteCard= new FavoriteCard(favorite);
-        favoriteCard.initialize();
-      })
-    }
-
-    // this is the only function that is available to the outside world.
-    return {
-      linkListener 
-    };
-  }());
+      //  this is the only function that is available to the outside world.
+      
+      return {
+        linkListener 
+      };
+    }());
 
   return Favorites;
 });
--- public/scripts/movie-form.js
+++ public/scripts/movie-form.js
@@ -3,7 +3,10 @@ define(function (require) {
   var PageManager= require('page-manager')
   var MovieCard= require('movie-card')
 
-  var MovieForm = function(){
+  var MovieForm = (function(){
+    // this performs a similar function to Favorite.linkListener. 
+    // In fact, it's so similar we could probably extract it into PageManager
+    
     var searchButtonListener = function(){
       document.getElementById("submit-search").onclick = function() { 
         var movieTitle = captureSearchInput();
@@ -13,9 +16,14 @@ define(function (require) {
       };
     };
 
+    // clearInput finds an element by it's id and sets its value to an empty string.
+    
     var clearInput= function(){
       document.getElementById("movie-input").value = "";
     }
+
+    //  injectMovieCards takes a list of favorite movies and instantiates an 
+    //  instance of FavoriteCard. It then calls initialize on the instance.
 
     var injectMovieCards= function(movies){
       movies.forEach(function(movie){
@@ -24,11 +32,18 @@ define(function (require) {
       })
     };
 
+    //  submitSearchRequest makes the Api module call a function by the same 
+    //  name and then waits for a promise (indicated by the then). upon 
+    //  succesful return of the promise it runs the function encapsulated 
+    //  by the then.
+    
     var submitSearchRequest = function(movieTitle){
       Api.submitSearch(movieTitle).then(function(response){
         injectMovieCards(response.Search)
       });
     }
+
+    //  captureSearchInput grabs the value of the input and substitutes every instance of a space with a plus in order to comply with what the url should look like.
 
     var captureSearchInput = function(){
       return document.getElementById("movie-input").value.replace(' ', '+')
@@ -37,7 +52,7 @@ define(function (require) {
     return {
       searchButtonListener
     }
-  }();
+  }());
 
   return MovieForm;
 });
